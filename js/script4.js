77963ce790f027a6f82572cbc3cee48f8eb0dd9f
Mikulas Vesely <mikulas.vesely@cdn77.com>
2022-05-12 09:53:35 +0200

    first commit

---
+++ js/script4.js
@@ -0,0 +1,73 @@
+/*  ---------------------------------------------------
+    Template Name: Europa Hotel And Spa
+    Description: Europa Hotel And Spa HTML Template
+    Author: Colorlib
+    Author URI: http://www.colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    /*------------------
+		Date Picker
+	--------------------*/
+    $(".datepicker-1").datepicker();
+    $(".datepicker-2").datepicker();
+
+    /*------------------
+        Milestone Counter
+    --------------------*/
+    $('.counter').each(function () {
+        $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+            $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+    /*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+})(jQuery);
